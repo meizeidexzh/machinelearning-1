4647d3d9e66ff9431e23f256906e6c6c105431cc
Yang Yi <ab045988yangyi@ab-insurance.com>
2018-01-12 16:04:49 +0800

    modified:   deeptesla.ipynb 	modified:   deeptesla.py

--- deep_tesla/deeptesla.py
+++ deep_tesla/deeptesla.py
@@ -4,12 +4,14 @@
 # In[1]:
 
 
+from os.path import isfile, isdir
+import csv
+import pickle
+
 #读取视频文件
 def loadmkv(file):
     import cv2
-    import csv
-    epochs_path = './epochs/'
-    cap = cv2.VideoCapture(epochs_path + file +'_front.mkv')
+    cap = cv2.VideoCapture(file +'_front.mkv')
     ret = True
     frame_list = []
     dict = {}
@@ -25,31 +27,27 @@ def loadmkv(file):
     dict['data']=frame_list
     cap.release()
     cv2.destroyAllWindows()
+    
     labels_list = []
-    with open(epochs_path + file + '_steering.csv', encoding='utf-8') as csvfile:
+    with open(file + '_steering.csv', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
             labels_list.append(row['wheel'])
         
     dict['labels']=labels_list
+   
     return dict
 
+epochs_path = '/epochs/'
 for i in range(1,2):
-    dict = loadmkv('epoch0' + str(i))
-print(dict['labels'][0])
+    filename = epochs_path + 'epoch0' + str(i)
+    outputfile = '/output/' + 'epoch0' + str(i)
+    if not isfile(filename + '.p'):
+        dict = loadmkv(filename)
+        pickle.dump((dict['data'], dict['labels']), open(outputfile + '.p', 'wb'))
+print('load file completion')
 
 
-# In[25]:
 
 
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-dict = unpickle('C:\\machinelearning\\image-classification\\cifar-10-batches-py\\data_batch_1')
-print(dict.keys())
-print(dict[b'data'][0],dict[b'labels'][0],dict[b'filenames'][0],dict[b'batch_label'][0])
-print(dict[b'data'][1],dict[b'labels'][1],dict[b'filenames'][1],dict[b'batch_label'][1])
 
